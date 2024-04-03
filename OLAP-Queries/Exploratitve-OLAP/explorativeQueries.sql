3b0942857ca3c6d107dd3a821c3f9e8975898a9e
Dana! <106605551+DanaShayakhmetova@users.noreply.github.com>
2024-04-02 21:54:06 -0400

    Update explorativeQueries.sql

--- OLAP-Queries/Exploratitve-OLAP/explorativeQueries.sql
+++ OLAP-Queries/Exploratitve-OLAP/explorativeQueries.sql
@@ -34,8 +34,17 @@ WHERE c.Education IN ('PhD', 'Master')
 ORDER BY c.Customer_Key;
 
 
-
 -- Combination 4: Roll up and Dice
+SELECT *,
+       CASE
+         WHEN (AGE_IN_2014 >= 20 AND AGE_IN_2014 < 30) THEN '20s'
+         WHEN (AGE_IN_2014 >= 30 AND AGE_IN_2014 < 40) THEN '30s'
+         WHEN (AGE_IN_2014 >= 40 AND AGE_IN_2014 < 50) THEN '40s'
+         WHEN (AGE_IN_2014 >= 50 AND AGE_IN_2014 < 60) THEN '50s'
+         WHEN (AGE_IN_2014 >= 60 AND AGE_IN_2014 < 70) THEN '60s'
+         ELSE '70+'
+       END AS Age_Group
+FROM Customer;
 
 
 
